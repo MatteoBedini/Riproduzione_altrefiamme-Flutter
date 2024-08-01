3287260b8f3d22f27520c114d25c950c9ac54b62
MatteoBedini <matteo.bedini.94@gmail.com>
2024-08-01 16:57:46 +0200

    keycloak begin

--- lib/pages/homepage/tabs/tab1-implant/implant.dart
+++ lib/pages/homepage/tabs/tab1-implant/implant.dart
@@ -4,30 +4,57 @@ import 'widgets/name_detail_section.dart';
 import 'widgets/errors_check.dart';
 import 'widgets/activation_check.dart';
 import 'widgets/total_ignition_time.dart';
+import 'package:http/http.dart' as http;
 
-
-
-class Implant extends StatelessWidget {
+class Implant extends StatefulWidget {
   final TabController tabController;
   const Implant({super.key, required this.tabController});
 
+  @override
+  State<Implant> createState() => _ImplantState();
+}
 
+class _ImplantState extends State<Implant> {
+  String username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _setAttributes(); // Fetch the username when the widget is initialized
+  }
+
+  Future<String> _fetchAttributes() async {
+    final response =
+        await http.get(Uri.parse('http://10.0.2.2:8701/user/name'));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return 'Failed to load name';
+    }
+  }
+
+  Future<void> _setAttributes() async {
+    final userName = await _fetchAttributes();
+    setState(() {
+      username = userName;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: EdgeInsets.zero,
       child: Container(
-
         padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               padding: const EdgeInsets.symmetric(vertical: 20),
-              child: const Text(
-                'Ciao utente',
-                style: TextStyle(
+              child: Text(
+                'Ciao $username',
+                style: const TextStyle(
                   color: Colors.black,
                   fontSize: 35,
                   fontWeight: FontWeight.bold,
@@ -35,11 +62,14 @@ class Implant extends StatelessWidget {
               ),
             ),
             const Text('Il tuo impianto'),
-            Name_detail_section(tabController: tabController,),
+            Name_detail_section(
+              tabController: widget.tabController,
+            ),
             const Errors_check(),
             const Activation_check(),
-            TotalIgnitionTime(tabController: tabController,),
-
+            TotalIgnitionTime(
+              tabController: widget.tabController,
+            ),
             const BookInterventionButton(),
           ],
         ),
@@ -47,6 +77,3 @@ class Implant extends StatelessWidget {
     );
   }
 }
-
-
-
