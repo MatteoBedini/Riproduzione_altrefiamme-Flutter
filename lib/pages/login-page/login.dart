9fdb1ce44ba9487adc2446bfc3f0143b200cf59e
Matteo Bedini <matteo.bedini.94@gmail.com>
2024-08-06 16:21:12 +0200

    registrazione completata

--- lib/pages/login-page/login.dart
+++ lib/pages/login-page/login.dart
@@ -78,87 +78,152 @@ class _LoginState extends State<Login> {
           color: Colors.red,
           child: Column(
             children: [
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 70, 0, 100),
-
-                child: Image.asset( 'assets/images/logo.png',color: Colors.white,scale: 0.5,),
-              ),
-              const Center(
-                child: Text('Login',style: TextStyle(color: Colors.white,fontSize: 40),),
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-
-                child:
-               Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Indirizzo mail',style: TextStyle(color: Colors.white,fontSize: 20),),
-                    TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
-
-                child:
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Password',style: TextStyle(color: Colors.white,fontSize: 20),),
-                    TextField(
-                      controller: passwordController,
-                      decoration: const InputDecoration(
-
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Center(
-                child: ElevatedButton(
-                  style: const ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Colors.red),
-                    foregroundColor: WidgetStatePropertyAll(Colors.white),
-                  ),
-                  onPressed: () {
-                    login(emailController.text, passwordController.text);
-                  },
-                  child: const Text('ACCEDI',style: (TextStyle(fontSize: 20)),),
-                ),
-              ),
-              Center(
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                        builder: (context) => const Register(),
-                    )
-                    );
-                  },
-                  child: const Text('Non sei registrato? Clicca qui',style: TextStyle(color: Colors.white,fontSize: 20,decoration: TextDecoration.underline,decorationColor: Colors.white),),
-                ),
-              )
+              const Logo(),
+              const Title(),
+              Mail(emailController: emailController),
+              Password(passwordController: passwordController),
+              _submit(),
+              const RedirectToRegister()
             ]
           )
         ),
       ),
+    );
+  }
+
+  Center _submit() {
+    return Center(
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.red),
+                  foregroundColor: WidgetStatePropertyAll(Colors.white),
+                ),
+                onPressed: () {
+                  login(emailController.text, passwordController.text);
+                },
+                child: const Text('ACCEDI',style: (TextStyle(fontSize: 20)),),
+              ),
+            );
+  }
+}
+
+class RedirectToRegister extends StatelessWidget {
+  const RedirectToRegister({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+              builder: (context) => const Register(),
+          )
+          );
+        },
+        child: const Text('Non sei registrato? Clicca qui',style: TextStyle(color: Colors.white,fontSize: 20,decoration: TextDecoration.underline,decorationColor: Colors.white),),
+      ),
+    );
+  }
+}
+
+class Password extends StatelessWidget {
+  const Password({
+    super.key,
+    required this.passwordController,
+  });
+
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+
+      child:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Password',style: TextStyle(color: Colors.white,fontSize: 20),),
+          TextField(
+            controller: passwordController,
+            decoration: const InputDecoration(
+
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Mail extends StatelessWidget {
+  const Mail({
+    super.key,
+    required this.emailController,
+  });
+
+  final TextEditingController emailController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+
+      child:
+     Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Indirizzo mail',style: TextStyle(color: Colors.white,fontSize: 20),),
+          TextField(
+            controller: emailController,
+            decoration: const InputDecoration(
+
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Login',style: TextStyle(color: Colors.white,fontSize: 40),),
+    );
+  }
+}
+
+class Logo extends StatelessWidget {
+  const Logo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 70, 0, 100),
+
+      child: Image.asset( 'assets/images/logo.png',color: Colors.white,scale: 0.5,),
     );
   }
 }
--- lib/pages/register_page/register.dart
+++ lib/pages/register_page/register.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:riproduzione_app_altrefiamme/pages/homepage/homepage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import '../login-page/login.dart';
 
@@ -14,128 +16,403 @@ class Register extends StatelessWidget {
         child: Container(
             padding: const EdgeInsets.all(40),
             color: Colors.red,
-            child: Column(
+            child: const Column(
                 children: [
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 70, 0, 100),
-
-                    child: Image.asset(
-                      'assets/images/logo.png', color: Colors.white,
-                      scale: 0.5,),
-                  ),
-                  const Center(
-                    child: Text('Registrati',
-                      style: TextStyle(color: Colors.white, fontSize: 40),),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-
-                    child:
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Indirizzo mail',
-                          style: TextStyle(color: Colors.white, fontSize: 20),),
-                        TextField(
-                          decoration: InputDecoration(
-
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(
-                                  20)),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-
-                    child:
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Password',
-                          style: TextStyle(color: Colors.white, fontSize: 20),),
-                        TextField(
-                          decoration: InputDecoration(
-
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(
-                                  20)),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
-
-                    child:
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Conferma Password',
-                          style: TextStyle(color: Colors.white, fontSize: 20),),
-                        TextField(
-                          decoration: InputDecoration(
-
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(
-                                  20)),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Center(
-                    child: ElevatedButton(
-                      style: const ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll(Colors.red),
-                        foregroundColor: WidgetStatePropertyAll(Colors.white),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const MyHomePage(
-                                  title: 'Riproduzione Altrefiamme')),
-                        );
-                      },
-                      child: const Text(
-                        'REGISTRATI', style: (TextStyle(fontSize: 20)),),
-                    ),
-                  ),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Login()),
-                          );
-                        }
-                      },
-                      child: const Text('Sei già registrato? Clicca qui',
-                        style: TextStyle(color: Colors.white,
-                            fontSize: 20,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.white),),
-                    ),
-                  )
+                  Logo(),
+                  Title(),
+                  Form(),
+                  AlreadyRegisteredRedirect()
                 ]
             )
         ),
       ),
+    );
+  }
+}
+
+class Form extends StatefulWidget {
+  const Form({
+    super.key,
+  });
+
+  @override
+  State<Form> createState() => _FormState();
+}
+
+class _FormState extends State<Form> {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  final nameController = TextEditingController();
+  final surnameController = TextEditingController();
+
+  Future<void> register(String email, String password, String name, String surname) async {
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2:8701/signup'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': email,
+        'password': password,
+        'name': name,
+        'surname': surname
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      if(passwordController.text == confirmPasswordController.text) {
+        // Login riuscito, naviga alla homepage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const Login(),
+          ),
+        );
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Registrazione completata'),
+              content: const Text('Utente inserito con successo, effettua il login'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        // Login fallito, mostra un messaggio di errore
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Registrazione fallita'),
+              content: const Text('Le password non corrispondono'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+
+    } else {
+      // Login fallito, mostra un messaggio di errore
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Registrazione fallita'),
+            content: const Text('Qualcosa è andato storto'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Name(nameController: nameController),
+        Surname(surnameController: surnameController),
+        Mail(emailController: emailController,),
+        Password(passwordController: passwordController),
+        ConfirmPassword(confirmPasswordController: confirmPasswordController),
+
+        _submit(),
+      ],
+    );
+  }
+
+  Center _submit() {
+    return Center(
+      child: ElevatedButton(
+        style: const ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll(Colors.red),
+          foregroundColor: WidgetStatePropertyAll(Colors.white),
+        ),
+        onPressed: () {
+          register(emailController.text, passwordController.text, nameController.text, surnameController.text);
+        },
+        child: const Text('ACCEDI',style: (TextStyle(fontSize: 20)),),
+      ),
+    );
+  }
+}
+
+class AlreadyRegisteredRedirect extends StatelessWidget {
+  const AlreadyRegisteredRedirect({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const Logo()),
+            );
+          }
+        },
+        child: const Text('Sei già registrato? Clicca qui',
+          style: TextStyle(color: Colors.white,
+              fontSize: 20,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.white),),
+      ),
+    );
+  }
+}
+
+
+
+class ConfirmPassword extends StatelessWidget {
+  const ConfirmPassword({
+    required this.confirmPasswordController,
+    super.key,
+  });
+
+
+  final TextEditingController confirmPasswordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+
+      child:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          const Text('Conferma Password',
+            style: TextStyle(color: Colors.white, fontSize: 20),),
+          TextField(
+            controller: confirmPasswordController,
+            decoration: const InputDecoration(
+
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(
+                    20)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Password extends StatelessWidget {
+  const Password({
+    super.key,
+    required this.passwordController,
+  });
+
+
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+
+      child:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Password',
+            style: TextStyle(color: Colors.white, fontSize: 20),),
+          TextField(
+            controller: passwordController,
+            decoration: const InputDecoration(
+
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(
+                    20)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Mail extends StatelessWidget {
+  const Mail({
+    super.key,
+    required this.emailController,
+  });
+
+  final TextEditingController emailController;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+
+      child:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Indirizzo mail',
+            style: TextStyle(color: Colors.white, fontSize: 20),),
+          TextField(
+            controller: emailController,
+            decoration: const InputDecoration(
+
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(
+                    20)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+class Name extends StatelessWidget {
+
+
+  const Name({
+    super.key,
+    required this.nameController,
+  });
+
+
+  final TextEditingController nameController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+
+      child:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Name',
+            style: TextStyle(color: Colors.white, fontSize: 20),),
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(
+
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(
+                    20)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Surname extends StatelessWidget {
+
+
+  const Surname({
+    super.key,
+    required this.surnameController,
+  });
+
+
+  final TextEditingController surnameController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+
+      child:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Surname',
+            style: TextStyle(color: Colors.white, fontSize: 20),),
+          TextField(
+            controller: surnameController,
+            decoration: const InputDecoration(
+
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(
+                    20)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Registrati',
+        style: TextStyle(color: Colors.white, fontSize: 40),),
+    );
+  }
+}
+
+class Logo extends StatelessWidget {
+  const Logo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 70, 0, 100),
+
+      child: Image.asset(
+        'assets/images/logo.png', color: Colors.white,
+        scale: 0.5,),
     );
   }
 }
